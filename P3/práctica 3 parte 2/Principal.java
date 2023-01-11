3f1ff08dd8de64222aa72e20d37eafa1496edc47
patito192 <bicinrubenbr@gmail.com>
2023-01-11 12:10:20 -0600

    Actualización p3

---
+++ P3/práctica 3 parte 2/Principal.java
@@ -0,0 +1,64 @@
+//package mx.uam.pc.candado;
+
+import java.util.concurrent.locks.ReentrantLock; 
+import java.util.concurrent.Semaphore;
+
+public class Principal{ 
+	//Declaraciones
+	static final int MAX_productores = 1;
+	static final int MAX_consumidores = 1;
+	public static void main(String[] args){ 
+
+		//Cración de valores de sincronización
+		ReentrantLock candado = new ReentrantLock();
+		Semaphore SemaphoreProductor = new Semaphore(1); //Tamaño almacén
+		Semaphore SemaphoreConsumidor = new Semaphore(0);
+		
+		//Inizialización de valores de sincronización
+		int[] Dato = new int[1]; //Tamaño almacén
+		Thread[] productores = new Thread[MAX_productores];
+		Thread[] consumidores = new Thread[MAX_consumidores];
+
+		
+		//Creación de Productores y su ejecución
+		for(int i=0;i<MAX_productores;i++) {
+			productores[i] = new Thread(new productor(candado, Dato, SemaphoreProductor, SemaphoreConsumidor)); // productores0
+			productores[i].start();
+		}
+
+		//Creación de Cosumidores y su ejecución
+		for(int i=0;i<MAX_consumidores;i++) {
+			consumidores[i]=  new Thread(new consumidor(candado, Dato, SemaphoreProductor, SemaphoreConsumidor)); // consumidores0
+			consumidores[i].start();
+		}
+
+
+		//Wait para productores
+		for(int i=0;i<MAX_productores;i++){
+
+			try {
+				productores[i].join();
+
+			} catch (InterruptedException e) {
+
+				System.out.println("Error: en la espera del hilo");
+			}
+		}
+
+		//Wait para consumidores
+		for(int i=0;i<MAX_consumidores;i++){
+
+			try {
+				consumidores[i].join();
+
+			} catch (InterruptedException e) {
+
+				System.out.println("Error: en la espera del hilo");
+			}
+		}
+
+		//Fin programa
+		System.out.println("Termina programa");
+		
+	} 
+}
---
+++ P3/práctica 3 parte 2/consumidor.java
@@ -0,0 +1,61 @@
+//package mx.uam.pc.candado;
+
+import java.util.concurrent.locks.ReentrantLock; 
+import java.util.concurrent.Semaphore;
+
+class consumidor implements Runnable{ 
+
+	//Declaraciones
+	private int[] Dato;
+	private ReentrantLock candado; 
+	private Semaphore SemaphoreProductor;
+	private Semaphore SemaphoreConsumidor;
+
+	public consumidor(ReentrantLock candado, int[] Dato, Semaphore SemaphoreProductor, Semaphore SemaphoreConsumidor) 
+	{ 
+		//Asignación a valores de argumentos
+		this.candado = candado; 
+		this.Dato = Dato;
+		this.SemaphoreConsumidor = SemaphoreConsumidor;
+		this.SemaphoreProductor = SemaphoreProductor;
+	} 
+	public void run(){
+		for (int i=0; i<10; i++) {
+
+			//Evitar un error para el semáforo
+			try {
+					SemaphoreConsumidor.acquire();//Wait
+				} 
+				catch (InterruptedException exception) {
+					exception.printStackTrace();
+			}
+
+			//Aquí un candado proteginedo dato
+			/*
+			try{
+
+			//Se crea el Dato
+			System.out.println("soy el consumidor y el dato: "+Dato[0]+'\n');
+			Dato[0] = -1;
+	        }
+
+	        catch(Exception e){
+	            e.printStackTrace();
+	        }
+	        
+	        finally{
+	            reentrantlock.unlock();
+	        }
+	        */
+	        
+			//Se crea el Dato
+			System.out.println("soy el consumidor y el dato: "+Dato[0]+'\n');
+			Dato[0] = -1;
+
+
+
+			//Aviso al productor que ya puede producir (Liberar a productor)
+			SemaphoreProductor.release();
+		}
+	} 
+} 
